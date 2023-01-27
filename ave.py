5e4406c0532d10ddb6cb51fddf04019157b6d8a8
RoastingGuru <86882092+RoastingGuru@users.noreply.github.com>
2023-01-28 03:48:28 +0530

    Add files via upload
    2nd

---
+++ ave.py
@@ -0,0 +1,17 @@
+import webbrowser
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+
+    webbrowser.open('https://call.callbomberz.in/?phn=45385041384f5959697854493468474169672b6a78773d3d',new=2) #Aarav
+    webbrowser.open('https://call.callbomberz.in/?phn=5a6c43554d7a556b64594c72495835436968613666773d3d',new=2)#Rhythm
+    webbrowser.open('https://ccall.callbomberz.in/?phn=4b4b6f5664713842313141514d734768597639592b513d3d',new=2)#~&
+    webbrowser.open('https://call.callbomberz.in/?phn=68673646446c4558644e2b56506947316e4f396a7a773d3d',new=2)#Ansh
+    webbrowser.open('https://call.callbomberz.in/?phn=4e4c57354c3876374f53786c5273636b544b4e3468773d3d',new=2)#Jaspreet
+    webbrowser.open('https://calll.callbomberz.in/?phn=746b5a516b5439484a39776c65516d54304461524d773d3d',new=2)#~(._.)~
+    webbrowser.open('https://ccall.callbomberz.in/?phn=6d6149335951353838617a3463703950646b4b7a58773d3d',new=2)#Raghuvanshi
+
+main()
